953467e38d866f15cac108d4b78a9b23c23557e4
ryan <ryan@better-logic.org>
2023-02-16 14:26:54 +0900

    this may confilct my remote but added bunch of location.codes modified guideTally to cope with scrape demand

--- guideTally.sh
+++ guideTally.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 path=`pwd`
-#loc=${1:-kyoto}
+list=${1:-overseas}
+
 
 echo location, service, guides
-for loc in `ls -1 ${path}/data/|cut -d'.' -f1`
+for loc in `ls -1 ${path}/data/${list}/|cut -d'.' -f1`
 do
-    guidecount=`cat ${path}/data/${loc}.data |cut -d',' -f1-2  |sort |uniq |wc -l`
-    servicecount=`cat ${path}/data/${loc}.data |cut -d',' -f1-2  |sort |wc -l`
+    guidecount=`cat ${path}/data/${list}/${loc}.data |cut -d',' -f1-2  |sort |uniq |wc -l`
+    servicecount=`cat ${path}/data/${list}/${loc}.data |cut -d',' -f1-2  |sort |wc -l`
     echo ${loc},${servicecount},${guidecount}
 done
 
