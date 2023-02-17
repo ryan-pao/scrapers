b65102a35a0f79c67939e8b55517c3be08f3dbae
ryan <ryan@better-logic.org>
2023-02-17 20:26:34 +0900

    ignore added tbl-scrape now runs via destination list

--- tbl/tbl-scrape.sh
+++ tbl/tbl-scrape.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
-location=${1:-kyoto}
-code=${2:-610}
+export LANG=C
+list=${1:-overseas}
+
+for x in `cat ./destinations/${list}.list`;
+do
+
+    location=`echo $x|cut -d':' -f1`;
+    code=`echo $x|cut -d':' -f2`;
+
 
 
 
@@ -12,7 +19,7 @@ echo location:$location
 # [733]For up to
 
 #echo  "${location},\\r\\n" > ${location}.data
-echo  "" > ${location}.data
+echo  "" > ./data/${list}/${location}.data
 
 lynx -dump https://www.toursbylocals.com/find_tour\&area=$code\&p=10|grep -A2 "USD per tour"| 
 sed "s/,//g" |
@@ -25,28 +32,28 @@ sed "s/--//g" |
 sed "s/hour.*$/&,----/g" |sed "s/hour.//g"|sed "s/30min./\.5/g"|sed "s/10min./\.00xx/g"|
 
 sed "s/days$/&,----/g" |
-sed '/^$/d' > $location.data
+sed '/^$/d' > ./data/${list}/$location.data
 
-cat ${location}.data |sed '/^$/d'|tr -d '\n' | sed "s/----/\r\n/g"  >$location.final
+cat ./data/${list}/${location}.data |sed '/^$/d'|tr -d '\n' | sed "s/----/\r\n/g"  > ./data/${list}/$location.final
 #lynx -dump https://www.toursbylocals.com/find_tour\&area=$code\&p=10 > ${location}.raw
-lynx -dump https://www.toursbylocals.com/find_tour\&area=$code\&p=10|grep "Your Guide:"|sed "s/^.*Your Guide: /${location},'/g"|sed "s/ /\./g"|sed "s/$/'/g"  > $location.guide
+lynx -dump https://www.toursbylocals.com/find_tour\&area=$code\&p=10|grep "Your Guide:"|sed "s/^.*Your Guide: /${location},'/g"|sed "s/ /\./g"|sed "s/$/'/g"  > ./data/${list}/$location.guide
 
-paste -d',' ${location}.guide ${location}.final > ./data/${location}.data
-sed -i "" "s/2days,/16,/g" ./data/${location}.data
-sed -i "" "s/3days,/24,/g" ./data/${location}.data
-sed -i "" "s/4days,/32,/g" ./data/${location}.data
-sed -i "" "s/5days,/40,/g" ./data/${location}.data
-sed -i "" "s/6days,/48,/g" ./data/${location}.data
-sed -i "" "s/7days,/56,/g" ./data/${location}.data
-sed -i "" "s/8days,/64,/g" ./data/${location}.data
-sed -i "" "s/9days,/72,/g" ./data/${location}.data
-sed -i "" "s/10days,/80,/g" ./data/${location}.data
-sed -i "" "s/11days,/88,/g" ./data/${location}.data
-sed -i "" "s/12days,/96,/g" ./data/${location}.data
-sed -i "" "s/13days,/104,/g" ./data/${location}.data
-sed -i "" "s/14days,/112,/g" ./data/${location}.data
+paste -d',' ./data/${list}/${location}.guide ./data/${list}/${location}.final > ./data/${list}/${location}.data
+sed -i "" "s/2days,/16,/g" ./data/${list}/${location}.data
+sed -i "" "s/3days,/24,/g" ./data/${list}/${location}.data
+sed -i "" "s/4days,/32,/g" ./data/${list}/${location}.data
+sed -i "" "s/5days,/40,/g" ./data/${list}/${location}.data
+sed -i "" "s/6days,/48,/g" ./data/${list}/${location}.data
+sed -i "" "s/7days,/56,/g" ./data/${list}/${location}.data
+sed -i "" "s/8days,/64,/g" ./data/${list}/${location}.data
+sed -i "" "s/9days,/72,/g" ./data/${list}/${location}.data
+sed -i "" "s/10days,/80,/g" ./data/${list}/${location}.data
+sed -i "" "s/11days,/88,/g" ./data/${list}/${location}.data
+sed -i "" "s/12days,/96,/g" ./data/${list}/${location}.data
+sed -i "" "s/13days,/104,/g" ./data/${list}/${location}.data
+sed -i "" "s/14days,/112,/g" ./data/${list}/${location}.data
 
-rm -rf ${location}.guide ${location}.final ${location}.data
+rm -rf ./data/${list}/${location}.guide ./data/${list}/${location}.final ${location}.data
 
 
-
+done
