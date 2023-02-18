08274a313e0f04e543b64dc477eea53fefdb60f0
ryan <ryan@better-logic.org>
2023-02-18 11:15:54 +0900

    beautified output for guideLocal and modified to render around lists

--- tbl/guideLocal.sh
+++ tbl/guideLocal.sh
@@ -1,13 +1,18 @@
 #! /bin/sh
+
 path=`pwd`
+list=${1:-japan}
 
 
-for guide in `cat ./data/*.data|cut -d',' -f2|sort|uniq`
+for guide in `cat ./data/${list}/*.data|cut -d',' -f2|sort|uniq`
     do
 #        echo $guide
 
-        cnt=`grep  -ls "$guide" ./data/*.data|wc -l`
-        loc=`grep  -ls "$guide" ./data/*.data |tr -d '\n' |sed "s/\.\//:/g"|cut -d ':' -f2- |sed "s/\.data//g"|
-        sed "s/data\///g"|sed "s/ /:/g"`
-        echo "$guide",$cnt,$loc
+        cnt=`grep  -ls "$guide" ./data/${list}/*.data|wc -l|tr -d ' '`
+        loc=`grep  -ls "$guide" ./data/${list}/*.data | tr -d '\n' |sed "s/\.\//,/g"|cut -d ':' -f2- |sed "s/${list}\///g"| sed "s/\.data//g"| sed "s/data\///g"|sed "s/ /:/g"`
+        #echo $loc|sed "s/^,//g"
+        spot=`echo $loc|sed "s/^,//g"`
+
+        echo "$guide",$cnt,$spot
+
     done
