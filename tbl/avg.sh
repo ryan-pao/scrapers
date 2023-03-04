c38cd03fdbc2cf1d62265311b8bd931c07c1a77f
ryan <ryan@better-logic.org>
2023-03-04 16:02:17 +0900

    quick average checker

--- tbl/avg.sh
+++ tbl/avg.sh
@@ -1,9 +1,18 @@
 #! /bin/sh
 
-raw=${1}
-echo raw ${raw}
-uno=`echo ${raw}| cut -d'-' -f1`
-dos=`echo ${raw}| cut -d'-' -f2`
-echo  "scale=0; ($uno+$dos)/2"|bc
-echo  "scale=0; ($uno+$dos)/2"|bc |tr -cd '[:print:]'|pbcopy
+target=${1:-Japan-Tours};
+path=`pwd`;
+total=`cat ./data/${target}/*.data |wc -l`
 
+
+for x in `cut -d',' -f3 ${path}/data/${target}/*.data`;
+do
+#   echo $x
+    num=$(( $num + ${x}))
+done
+
+
+
+#echo ${num}/${total}
+avg=`echo "scale=1; ${num}/${total}"| bc`;
+echo ${target}:${total} offered avg:${avg}
